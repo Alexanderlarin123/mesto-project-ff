f8b779746996b68f0818e00080c3848f76dd515c
Alexanderlarin111 <alexander.larin@halton.com>
2024-03-18 10:35:59 -0500

    Day3

--- scripts/index.js
+++ scripts/index.js
@@ -5,24 +5,26 @@
 const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-function createCard(link,name) {   
+function createCard(cardData,callback) { 
   const card=document.querySelector('#card-template').content.querySelector('.places__item').cloneNode(true);
-  card.querySelector('img').src=link;
-  card.querySelector('h2').textContent=name;
-  card.id=name;
+  card.querySelector('img').src=cardData['link'];
+  card.querySelector('img').alt=cardData['name'];
+  card.querySelector('h2').textContent=cardData['name'];;
   card.querySelector('.card__delete-button').addEventListener('click', function (){
-   deleteCard (card.id);
+   callback(card);
   });
   return card;
 }
 // @todo: Функция удаления карточки
-function deleteCard (id) {
-    const cardItem=document.getElementById(id);
-    if (cardItem){
-          cardItem.remove();
+function deleteCard (card) {
+   const cards=placesList.getElementsByTagName('li');
+    for (let i = 0; i < cards.length; i++) {
+    if (cards[i]==card){
+          card.remove();
     }
+  }
 }
 // @todo: Вывести карточки на страницу
 for (let i = 0; i < initialCards.length; i++) {
-    placesList.append(createCard(initialCards[i].link,initialCards[i].name));
+    placesList.append(createCard(initialCards[i],deleteCard));
   }
