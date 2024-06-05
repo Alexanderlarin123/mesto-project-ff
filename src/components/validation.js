4d69991870a05db72bbf33dd6ddd6a0a62bb6e27
Alexanderlarin111 <alexander.larin@halton.com>
2024-06-05 11:07:06 -0500

    sprint 7, day 3

---
+++ src/components/validation.js
@@ -0,0 +1,59 @@
+const showInputError = (formElement, inputElement, errorMessage) => {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.add('popup__input_type_error');
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add('popup__input-error_active');
+  };
+  const hideInputError = (formElement, inputElement) => {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.remove('popup__input_type_error');
+    errorElement.classList.remove('popup__input-error_active');
+    errorElement.textContent = '';
+  };
+  const checkInputValidity = (formElement, inputElement) => {
+    if (!inputElement.validity.valid) {
+      showInputError(formElement, inputElement, inputElement.validationMessage);
+    } else {
+      hideInputError(formElement, inputElement);
+    }
+  }; 
+  const setEventListeners = (formElement) => {
+    const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+    const buttonElement = formElement.querySelector('.popup__button');
+    // чтобы проверить состояние кнопки в самом начале
+    toggleButtonState(inputList, buttonElement);
+    inputList.forEach((inputElement) => {
+      inputElement.addEventListener('input', function () {
+        checkInputValidity(formElement, inputElement);
+        // чтобы проверять его при изменении любого из полей
+        toggleButtonState(inputList, buttonElement);
+      });
+    });
+  };  
+  const enableValidation = () => {
+    const formList = Array.from(document.querySelectorAll('.popup__form'));
+    formList.forEach((formElement) => {
+      formElement.addEventListener('submit', function (evt) {
+        evt.preventDefault();
+      });  
+      setEventListeners(formElement);
+    });
+  }; 
+  const hasInvalidInput = (inputList) => {
+    return inputList.some((inputElement) => {
+      return !inputElement.validity.valid;
+    })
+  };
+  const toggleButtonState = (inputList, buttonElement) => {
+    // Если есть хотя бы один невалидный инпут
+    if (hasInvalidInput(inputList)) {
+      // сделай кнопку неактивной
+          buttonElement.disabled = true;
+      buttonElement.classList.add('button_inactive');
+    } else {
+          // иначе сделай кнопку активной
+          buttonElement.disabled = false;
+      buttonElement.classList.remove('button_inactive');
+    }
+  };
+  export {enableValidation,hideInputError};
--- src/index.js
+++ src/index.js
@@ -14,28 +14,74 @@ import {
   openPopup,
   setClosePopupEventListeners
 } from "./components/modal.js"
+import {
+  enableValidation,
+  hideInputError
+} from "./components/validation.js"
 
 //DOM узлы
+const token = "883e0234-47f8-49e4-92ee-690f5802db36";
+const cohort = "wff-cohort-14";
 const profileEditButton = document.querySelector('.profile__edit-button');
 const newCardButton = document.querySelector('.profile__add-button');
 const placesList = document.querySelector('.places__list');
 const popupEdit = document.querySelector('.popup_type_edit');
+hideInputError(popupEdit,popupEdit.querySelector('input[name="description"]'));
 const popupNewCard = document.querySelector('.popup_type_new-card');
 
+getProfileInfo(token,cohort);
+//Get user information from server
+function getProfileInfo(token,cohort){
+fetch(`https://nomoreparties.co/v1/${cohort}/users/me`, {
+  headers: {
+    authorization:token
+  }
+})
+  .then(res => res.json())
+  .then((result) => {
+    updateProfileInfo(result.name,result.about,result.avatar)
+  }); 
+}
+
+function updateProfileInfo(name, about, avatar) {
+  document.querySelector('.profile__title').textContent=name;
+  document.querySelector('.profile__description').textContent=about;
+  document.querySelector('.profile__image').style=`background-image: url(${avatar})`;
+}
+
 //Вывести карточки на страницу
+fetch('https://nomoreparties.co/v1/wff-cohort-14/cards', {
+  headers: {
+    authorization: '883e0234-47f8-49e4-92ee-690f5802db36'
+  }
+})
+  .then(res => res.json())
+  .then((result) => {
+    console.log(result)
+   
+  }); 
+
 initialCards.forEach(function (element) {
   placesList.append(createCard(element, deleteCard, likeCard, viewCard));
 })
 
 //Add event listener for the buttons: profile_edit and add_new_card
 profileEditButton.addEventListener("click", (evt) => {
+  hideInputError(popupEdit,popupEdit.querySelector('input[name="name"]'));
+  hideInputError(popupEdit,popupEdit.querySelector('input[name="description"]'));
   openPopup(popupEdit);
   popupEdit.querySelector('input[name="name"]').value = document.querySelector('.profile__title').textContent;
   popupEdit.querySelector('input[name="description"]').value = document.querySelector('.profile__description').textContent;
 });
+
 newCardButton.addEventListener("click", (evt) => {
+  hideInputError(popupNewCard,popupNewCard.querySelector('input[name="place-name"]'));
+  hideInputError(popupNewCard,popupNewCard.querySelector('input[name="link"]'));
+  popupNewCard.querySelector('input[name="place-name"]').value = "";
+  popupNewCard.querySelector('input[name="link"]').value = "";
   openPopup(popupNewCard);
 })
+
 popupEdit.addEventListener('submit', handleFormSubmitEdit);
 popupNewCard.addEventListener('submit', handleFormSubmitAddNewCard);
 
@@ -75,3 +121,6 @@ function viewCard(name, link) {
 document.querySelectorAll('.popup').forEach(function (element) {
   setClosePopupEventListeners(element);
 });
+
+enableValidation();
+
